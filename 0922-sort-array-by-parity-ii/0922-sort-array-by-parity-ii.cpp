28aacc71c080d0cd859a2978b2a850ad5d26b469
karan1_2 <142498455+karankumar12345@users.noreply.github.com>
2026-02-16 19:08:49 +0530

    Time: 4 ms (10.02%), Space: 25.8 MB (73.49%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
-        
-        //iska logic simple check karu index agar uska element odd raha tu even index me swap kar dega are same opposite 
 
-        // In This Question we can simply have a check if even index have a odd numebr then  swap to even number 
+        // iska logic simple check karu index agar uska element odd raha tu even
+        // index me swap kar dega are same opposite
 
-        int n=nums.size();
-        int evenindex=0;
-        int oddindex=1;
-        while(evenindex<n && oddindex<n){
-            if(nums[evenindex]%2==0){
-                evenindex +=2;
-            }
-            else if(nums[oddindex]%2!=0){
-                oddindex+=2;
-            }
-            else{
-                swap(nums[evenindex],nums[oddindex]);
-                oddindex+=2;
-                evenindex+=2;
-                
+  
+        int n = nums.size();
+        int evenIndex = 0;
+        int oddIndex = 1;
+        while (evenIndex < n && oddIndex < n) {
+            if (nums[evenIndex] % 2 == 0) {
+                evenIndex += 2;
+            } else if (nums[oddIndex] % 2 != 0) {
+                oddIndex += 2;
+            } else {
+                swap(nums[oddIndex],nums[evenIndex]);
+                 oddIndex += 2;
+                evenIndex += 2;
             }
         }
         return nums;
