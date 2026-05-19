f50e058a2317ea516cc6d9b7fca386f06626ae2a
karan1_2 <142498455+karankumar12345@users.noreply.github.com>
2026-05-19 19:30:57 +0530

    Time: 0 ms (100%), Space: 10.1 MB (75.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,66 +1,67 @@
 class Solution {
 public:
     int Recu(vector<int>& nums, int n) {
-        if (n < 0)
+        if (n < 0) {
             return -1;
-        if (n == 0)
+        }
+        if (n == 0) {
             return nums[0];
-
+        }
         int incl = Recu(nums, n - 2) + nums[n];
         int excl = Recu(nums, n - 1) + 0;
-        int ans = max(incl, excl);
-        return ans;
+        return max(incl, excl);
     }
-
-    int Memo(vector<int>&nums,int n,vector<int>&dp){
-        if(n<0) return -1;
-        if(n==0) return nums[0];
-        if(dp[n]!=-1){
-            return  dp[n];
+    int TopDown(vector<int>&nums,int n, vector<int>&dp){
+          if (n == 1) {
+            return max(nums[0],nums[1]);
         }
-        int incl=Memo(nums,n-2,dp) + nums[n];
-        int excl=Memo(nums,n-1,dp)+0;
-        dp[n]=max(incl,excl);
+        if (n == 0) {
+            return nums[0];
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int incl=TopDown(nums,n-2,dp)+nums[n];
+        int excl=TopDown(nums,n-1,dp);
+        return max(incl,excl);
+    }
+
+    int BottomUp(vector<int>&nums,int n){
+        vector<int>dp(n+1,0);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+
+        for(int i=2;i<n;i++){
+            int incl=BottomUp(nums,n-2)+nums[i];
+            int excl=BottomUp(nums,n-1)+0;
+            dp[i]=max(incl,excl);
+        }
         return dp[n];
-
-    }
-    int solve2(vector<int>& nums, int n) {
-              vector<int>dp(n+1,0);
-    dp[0]=nums[0];
-    for(int i=1;i<=n;i++){
-             int incl = nums[i] + (i >= 2 ? dp[i - 2] : 0);
-        int excl=dp[i-1]+0;
-        dp[i]=max(incl,excl);
-    }
-    return dp[n];
     }
 
-
-    int SpaceOpt(vector<int>&nums,int n){
-              int   prev2=0;
-  int  prev1=nums[0];
-       for(int i=1;i<=n;i++){
-        int incl=prev2+nums[i];
-        int excl=prev1+0;
-        int ans=max(incl,excl);
-        prev2=prev1;
-        prev1=ans;
-       
-    }
-    return prev1;
-    }
     int rob(vector<int>& nums) {
 
         int n = nums.size();
-        // return Recu(nums, n - 1);
-        // vector<int>dp(n,-1);
-        // return Memo(nums,n-1,dp);
 
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-    //  int ans=solve2(nums,n-1);
-    //     return ans;
+        // prev2 = dp[i-2], prev1 = dp[i-1]
+        int prev2 = 0;
+        int prev1 = nums[0];
 
-    return  SpaceOpt(nums,n-1);
+        for (int i = 1; i < n; i++) {
 
+            int incl = nums[i] + prev2; // take current
+            int excl = prev1;           // skip current
+
+            int curr = max(incl, excl);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
+
 };
