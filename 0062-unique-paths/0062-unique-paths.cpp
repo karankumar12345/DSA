2b2f4cfc903a9e94a4b76e11135babb124daba84
karan1_2 <142498455+karankumar12345@users.noreply.github.com>
2026-05-21 07:39:17 +0530

    Time: 0 ms (100%), Space: 9.5 MB (28.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -17,17 +17,44 @@ public:
         if (n == 0 && m == 0) {
             return 1;
         }
-        if(dp[m][n]!=-1){
+        if (dp[m][n] != -1) {
             return dp[m][n];
         }
 
-        dp[m][n]=TopDown(m-1,n,dp)+TopDown(m,n-1,dp);
+        dp[m][n] = TopDown(m - 1, n, dp) + TopDown(m, n - 1, dp);
         return dp[m][n];
     }
+   int BottomUp(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+
+    dp[0][0] = 1;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (i == 0 && j == 0) continue;
+
+            int up = 0;
+            int left = 0;
+
+            if (i > 0)
+                up = dp[i - 1][j];
+
+            if (j > 0)
+                left = dp[i][j - 1];
+
+            dp[i][j] = up + left;
+        }
+    }
+
+    return dp[m - 1][n - 1];
+}
     int uniquePaths(int m, int n) {
         // return Recu(m - 1, n - 1);
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return TopDown(m - 1, n - 1, dp);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return TopDown(m - 1, n - 1, dp);
+
+        return BottomUp(m, n);
     }
 };
