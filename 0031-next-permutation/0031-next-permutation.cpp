1ab57873e102bce23b448651c090ad81d8c50f57
karan1_2 <142498455+karankumar12345@users.noreply.github.com>
2026-07-14 09:55:15 +0530

    Time: 0 ms (100%), Space: 15.9 MB (7.95%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,22 +3,34 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
 
-        int i = n - 2;
-
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        if (i >= 0) {
-            int j = n - 1;
-
-            while (nums[j] <= nums[i]) {
-                j--;
+        // Step 1: Find pivot
+        int reverse1 = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                reverse1 = i;
+                break;
             }
-
-            swap(nums[i], nums[j]);
         }
 
-        reverse(nums.begin() + i + 1, nums.end());
+        // Step 2: If no pivot, reverse whole array
+        if (reverse1 == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // Step 3: Find rightmost greater element
+        int index = n - 1;
+        for (int i = n - 1; i > reverse1; i--) {
+            if (nums[i] > nums[reverse1]) {
+                index = i;
+                break;
+            }
+        }
+
+        // Step 4: Swap
+        swap(nums[reverse1], nums[index]);
+
+        // Step 5: Reverse suffix
+        reverse(nums.begin() + reverse1 + 1, nums.end());
     }
 };
